c141dbc5fb7e0572678a63d5143438fb011f5e71
Baird Hendrix <baird.hendrix@luminartech.com>
2020-11-06 11:37:46 -0800

    Rewrote in asyncio and now it's working yay.  A LOT less complicated too.

--- supply_commander.py
+++ supply_commander.py
@@ -1,145 +1,150 @@
-import telnetlib
+import asyncio
+import telnetlib3
 import logging
 import pprint
+from queue import SimpleQueue as Queue
 
-log = logging.Logger('VCUHIL')
-log.setLevel('DEBUG')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M')
+log = logging.Logger('')
+
 
 def _trim_string(string):
-        return str(string).split('\\r\\n')[0]
+        return str(string).strip()
 
 
 class SorensenXPF6020DP(object):
-    def __init__(self, host, port):
-        self._host = host
-        self._port = port
-        self.psu_tel = None
+    def __init__(self):
+        self.reader = None
+        self.writer = None
 
-    def connect(self):
-        self.psu_tel = telnetlib.Telnet('psu-leonardo', 9221)
+    async def connect(self, host, port):
+        logging.debug('CONNECTING')
+        coro = telnetlib3.open_connection(host, port)
+        self.reader, self.writer = await coro
+        logging.debug('CONNECTED')
 
-    def readback(self):
+    async def readback(self):
         return {
             1: {
-                'meas_voltage':self.voltage_channel1_meas,
-                'meas_current':self.current_channel1_meas,
-                'set_voltage':self.voltage_channel1,
-                'set_current':self.current_channel1,
-                'output_enabled':self.output_channel1,
+                'meas_voltage':await self.get_voltage_channel1_meas(),
+                'meas_current':await self.get_current_channel1_meas(),
+                'set_voltage':await self.get_voltage_channel1(),
+                'set_current':await self.get_current_channel1(),
+                'output_enabled':await self.get_output_channel1(),
             },
             2: {
-                'meas_voltage':self.voltage_channel2_meas,
-                'meas_current':self.current_channel2_meas,
-                'set_voltage':self.voltage_channel2,
-                'set_current':self.current_channel2,
-                'output_enabled':self.output_channel2,
+                'meas_voltage':await self.get_voltage_channel2_meas(),
+                'meas_current':await self.get_current_channel2_meas(),
+                'set_voltage':await self.get_voltage_channel2(),
+                'set_current':await self.get_current_channel2(),
+                'output_enabled':await self.get_output_channel2(),
             }
         }
 
     def close(self):
-        self.psu_tel.close()
+        self.writer.close()
 
-    def _generic_command_response(self, command):
-        self._generic_command(command)
-        response = self.psu_tel.read_until(b'\r\n')
+    async def _generic_command_response(self, command):
+        logging.debug(f'WRITING: {command}')
+        self.writer.write(command)
+        response = await self.reader.readline()
+        logging.debug(f'RECV: {response}')
         return _trim_string(response)
 
     def _generic_command(self, command):
-        self.psu_tel.write(f'{command}\r\n'.encode('ascii'))
+        logging.debug(f'WRITIG: {command}')
+        self.writer.write(command)
 
-    def identify(self):
-        return self._generic_command_response('*IDN?')[2:]
+    async def identify(self):
+        return await self._generic_command_response('*IDN?')
 
-    @property
-    def voltage_channel1(self):
-        return float(self._generic_command_response(f'V1?')[5:])
+    async def get_voltage_channel1(self):
+        return float((await self._generic_command_response(f'V1?'))[3:])
 
-    @voltage_channel1.setter
-    def voltage_channel1(self, voltage):
+    def set_voltage_channel1(self, voltage):
         self._generic_command(f'V1 {float(voltage)}')
 
-    @property
-    def voltage_channel2(self):
-        return float(self._generic_command_response(f'V2?')[5:])
+    async def get_voltage_channel2(self):
+        return float((await self._generic_command_response(f'V2?'))[3:])
 
-    @voltage_channel2.setter
-    def voltage_channel2(self, voltage):
+    def set_voltage_channel2(self, voltage):
         self._generic_command(f'V2 {float(voltage)}')
 
-    @property
-    def current_channel1(self):
-        return float(self._generic_command_response(f'I1?')[5:])
+    async def get_current_channel1(self):
+        return float((await self._generic_command_response(f'I1?'))[3:])
 
-    @current_channel1.setter
-    def current_channel1(self, current):
+    def set_current_channel1(self, current):
         self._generic_command(f'I1 {float(current)}')
 
-    @property
-    def current_channel2(self):
-        return float(self._generic_command_response(f'I2?')[5:])
+    async def get_current_channel2(self):
+        return float((await self._generic_command_response(f'I2?'))[3:])
 
-    @current_channel2.setter
-    def current_channel2(self, current):
+    def set_current_channel2(self, current):
         self._generic_command(f'I2 {float(current)}')
 
-    @property
-    def voltage_channel1_meas(self):
-        return float(self._generic_command_response(f'V1O?')[2:-1])
+    async def get_voltage_channel1_meas(self):
+        return float((await self._generic_command_response(f'V1O?'))[:-1])
 
-    @property
-    def voltage_channel2_meas(self):
-        return float(self._generic_command_response(f'V2O?')[2:-1])
+    async def get_voltage_channel2_meas(self):
+        return float((await self._generic_command_response(f'V2O?'))[:-1])
 
-    @property
-    def current_channel1_meas(self):
-        return float(self._generic_command_response(f'I1O?')[2:-1])
+    async def get_current_channel1_meas(self):
+        return float((await self._generic_command_response(f'I1O?'))[:-1])
 
-    @property
-    def current_channel2_meas(self):
-        return float(self._generic_command_response(f'I2O?')[2:-1])
+    async def get_current_channel2_meas(self):
+        return float((await self._generic_command_response(f'I2O?'))[:-1])
 
-    @property
-    def output_channel1(self):
-        return bool(self._generic_command_response('OP1?')[2:])
+    async def get_output_channel1(self):
+        return bool((await self._generic_command_response('OP1?'))[:])
 
-    @output_channel1.setter
-    def output_channel1(self, boolean):
+    def set_output_channel1(self, boolean):
         self._generic_command(f'OP1 {int(boolean)}')
 
-    @property
-    def output_channel2(self):
-        return bool(self._generic_command_response('OP2?')[2:])
+    async def get_output_channel2(self):
+        return bool((await self._generic_command_response('OP2?'))[:])
 
-    @output_channel2.setter
-    def output_channel2(self, boolean):
+    def set_output_channel2(self, boolean):
         self._generic_command(f'OP2 {int(boolean)}')
 
-    def supply_state(self):
-        return pprint.pformat(self.readback())
+    async def supply_state(self):
+        logging.debug('Get Supply State')
+        return pprint.pformat(await self.readback())
+
+async def main():
+    # Setup
+    psu = SorensenXPF6020DP()
+
+    await psu.connect('psu-leonardo', 9221)
+
+    idn = await psu.identify()
+    print(idn)
+    print((await psu.supply_state()))
+    await asyncio.sleep(1)
+    psu.set_voltage_channel1(16.0)
+    psu.set_voltage_channel2(16.0)
+    psu.set_current_channel1(7.0)
+    psu.set_current_channel2(7.0)
+    psu.set_output_channel1(1)
+    psu.set_output_channel2(1)
+
+    await asyncio.sleep(1)
+    print((await psu.supply_state()))
+
+    await asyncio.sleep(1)
+    psu.set_voltage_channel1(0)
+    psu.set_voltage_channel2(0)
+    psu.set_current_channel1(0)
+    psu.set_current_channel2(0)
+    psu.set_output_channel1(0)
+    psu.set_output_channel2(0)
+
+    await asyncio.sleep(1)
+    psu.close()
+
 
 
 if __name__ == '__main__':
-    import time
-    psu = SorensenXPF6020DP('psu-leonardo', 9221)
-    psu.connect()
-    print(psu.identify())
-    print(psu.supply_state())
-    time.sleep(0.5)
-    psu.voltage_channel1 = 16.0
-    psu.voltage_channel2 = 16.0
-    psu.current_channel1 = 7.0
-    psu.current_channel2 = 7.0
-    psu.output_channel1 = 1
-    psu.output_channel2 = 1
-    time.sleep(1.5)
-    print(psu.supply_state())
-    time.sleep(2)
-    psu.voltage_channel1 = 0
-    psu.voltage_channel2 = 0
-    psu.current_channel1 = 0
-    psu.current_channel2 = 0
-    psu.output_channel1 = 0
-    psu.output_channel2 = 0
-    time.sleep(0.5)
-    psu.close()
+    asyncio.run(main())
 
