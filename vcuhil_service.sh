28b12b6f952d82cefbf4f5644854e8070b79e530
Baird Hendrix <baird.hendrix@luminartech.com>
2020-11-23 10:04:15 -0800

    Got VCU service to run correctly

--- vcuhil_service.sh
+++ vcuhil_service.sh
@@ -3,4 +3,4 @@
 export PATH="/home/vcuhil/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-eval "$(pipenv run python3 vcuhil_service.py)"
+exec pipenv run python3 vcuhil_service.py
