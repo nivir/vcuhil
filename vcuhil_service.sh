8b6def2a764165eb32e88061f0f42850e1c31246
Baird Hendrix <baird.hendrix@luminartech.com>
2020-11-23 11:33:17 -0800

    vcu_hil now logs to a reasonable place

--- vcuhil_service.sh
+++ vcuhil_service.sh
@@ -3,4 +3,4 @@
 export PATH="/home/vcuhil/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-exec pipenv run python3 vcuhil_service.py
+exec pipenv run python3 vcuhil_service.py --log_filename /luminar/vcuhil/log/vcuhil.json
